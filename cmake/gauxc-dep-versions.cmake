ce3a50e38e218754e2e89a0a11c35573f74c1ab7
Shiv Upadhyay <shivnupadhyay@gmail.com>
2026-01-20 19:50:15 -0500

    unpin deps

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -1,8 +1,5 @@
 set( GAUXC_LINALG_MODULES_REPOSITORY https://github.com/wavefunction91/linalg-cmake-modules.git )
-set( GAUXC_LINALG_MODULES_REVISION  28ceaa9738f14935aa544289fa2fe4c4cc955d0e )
-
-set( GAUXC_CEREAL_REPOSITORY https://github.com/USCiLab/cereal.git )
-set( GAUXC_CEREAL_REVISION   v1.3.0 )
+set( GAUXC_LINALG_MODULES_REVISION  9d2c273a671d6811e9fd432f6a4fa3d915b144b8 )
 
 set( GAUXC_CUB_REPOSITORY https://github.com/NVIDIA/cub.git )
 set( GAUXC_CUB_REVISION   1.10.0 )
@@ -11,13 +8,13 @@ set( GAUXC_CUTLASS_REPOSITORY https://github.com/NVIDIA/cutlass.git )
 set( GAUXC_CUTLASS_REVISION v2.10.0 )
 
 set( GAUXC_EXCHCXX_REPOSITORY https://github.com/wavefunction91/ExchCXX.git )
-set( GAUXC_EXCHCXX_REVISION   ef88e168249e38c526353fe7c48aab4bc697c99d )
+set( GAUXC_EXCHCXX_REVISION   v1.0.0 )
 
 set( GAUXC_GAU2GRID_REPOSITORY https://github.com/dgasmith/gau2grid.git )
 set( GAUXC_GAU2GRID_REVISION   v2.0.6 )
 
 set( GAUXC_INTEGRATORXX_REPOSITORY https://github.com/wavefunction91/IntegratorXX.git )
-set( GAUXC_INTEGRATORXX_REVISION   ea07dedd37e7bd49ea06394eb811599002b34b49 )
+set( GAUXC_INTEGRATORXX_REVISION   1369be58d7a3235dac36d75dd964fef058830622 )
 
-set( GAUXC_HIGHFIVE_REPOSITORY https://github.com/BlueBrain/HighFive.git )
+set( GAUXC_HIGHFIVE_REPOSITORY https://github.com/highfive-devs/HighFive.git )
 set( GAUXC_HIGHFIVE_REVISION 805f0e13d09b47c4b01d40682621904aa3b31bb8 )
